cb4de6c9fbdca9d3b4683cfc9275b41ebed92b3a
costinianoar <costinianoar@appstate.edu>
2025-04-14 17:13:50 -0400

    added name logo

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {ImageBackground, TextInput, View, StyleSheet, TouchableHighlight, Alert} from 'react-native';
+import {Image, ImageBackground, TextInput, View, StyleSheet, TouchableHighlight, Alert} from 'react-native';
 import ThemeText from '@/components/ui/ThemeText'
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 import { useRouter } from 'expo-router';
@@ -11,21 +11,26 @@ const HomeScreen = () => {
       <SafeAreaProvider>
         <SafeAreaView style={styles.container} edges={['left', 'right']}>
           <ImageBackground source={require('../assets/images/splash1-mobile.jpg')} resizeMode="cover" style={styles.image}>
-            <TouchableHighlight style={styles.buttonContainer} onPress={() => router.push('/login')}>
-              <View style={styles.button}>
-                <ThemeText style={styles.buttonText}>Returning User</ThemeText>
-              </View>
-            </TouchableHighlight>
-            <TouchableHighlight style={styles.buttonContainer} onPress={() => router.push('/signup')}>
-              <View style={styles.button}>
-                <ThemeText style={styles.buttonText}>New User</ThemeText>
-              </View>
-            </TouchableHighlight>
-            <TouchableHighlight style={styles.buttonContainer} onPress={() => router.push('/home')}>
-              <View style={styles.button}>
-                <ThemeText style={styles.buttonText}>Temp Button</ThemeText>
-              </View>
-            </TouchableHighlight>
+		  <View style={styles.logoContainer}>
+			<Image source={require('../assets/images/logo.png')} style={styles.logo} />
+		  </View>
+		  <View style={styles.buttonGroup}>
+			<TouchableHighlight style={styles.buttonContainer} onPress={() => router.push('/login')}>
+			<View style={styles.button}>
+				<ThemeText style={styles.buttonText}>Returning User</ThemeText>
+			</View>
+			</TouchableHighlight>
+			<TouchableHighlight style={styles.buttonContainer} onPress={() => router.push('/signup')}>
+			<View style={styles.button}>
+				<ThemeText style={styles.buttonText}>New User</ThemeText>
+			</View>
+			</TouchableHighlight>
+			<TouchableHighlight style={styles.buttonContainer} onPress={() => router.push('/home')}>
+			<View style={styles.button}>
+				<ThemeText style={styles.buttonText}>Temp Button</ThemeText>
+			</View>
+			</TouchableHighlight>
+		</View>
           </ImageBackground>
         </SafeAreaView>
       </SafeAreaProvider>
@@ -37,21 +42,42 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'grey',
   },
+  logoContainer: {
+	flex: 1,
+	justifyContent: 'center',
+	alignItems: 'center',
+	width: '100%',
+  },
+  logo: {
+	width: '150%',
+	height: undefined,
+	aspectRatio: 1,    
+	resizeMode: 'contain',
+	paddingTop: 200,
+  },
   image: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     width: '100%',
   },
+  buttonGroup: {
+	flex: 1,
+	justifyContent: 'flex-start',
+	alignItems: 'center',
+	paddingTop: 0, 
+  },
   buttonContainer: {
-    width: '80%',
+    width: '100%',
     marginTop: 20,
   },
   button: {
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#497941',
-    height: 40,
+    height: 50,
+	borderRadius: 6,
+	padding: 10,
   },
   buttonText: {
     fontWeight: 'bold', 
